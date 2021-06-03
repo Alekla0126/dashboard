e2e58f064b846822d70ca116a92e343443b94c8b
alekla0126 <alekla0126@gmail.com>
2021-06-03 18:54:41 -0500

    V1.3

--- resources/views/layouts/navbars/navs/guest.blade.php
+++ resources/views/layouts/navbars/navs/guest.blade.php
@@ -19,17 +19,17 @@
         </li>
         <li class="nav-item{{ $activePage == 'register' ? ' active' : '' }}">
           <a href="{{ route('register') }}" class="nav-link">
-            <i class="material-icons">person_add</i> {{ __('Register') }}
+            <i class="material-icons">person_add</i> {{ __('Registro') }}
           </a>
         </li>
         <li class="nav-item{{ $activePage == 'login' ? ' active' : '' }}">
           <a href="{{ route('login') }}" class="nav-link">
-            <i class="material-icons">fingerprint</i> {{ __('Login') }}
+            <i class="material-icons">fingerprint</i> {{ __('Inicio') }}
           </a>
         </li>
         <li class="nav-item ">
           <a href="{{ route('profile.edit') }}" class="nav-link">
-            <i class="material-icons">face</i> {{ __('Profile') }}
+            <i class="material-icons">face</i> {{ __('Perfil') }}
           </a>
         </li>
       </ul>
