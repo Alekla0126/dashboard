5140c1529273ac1f7e97a7b5ec52f62f83e72543
alekla0126 <alekla0126@gmail.com>
2021-06-03 19:03:10 -0500

    V1.6

--- resources/views/layouts/footers/guest.blade.php
+++ resources/views/layouts/footers/guest.blade.php
@@ -1,35 +1,20 @@
 <footer class="footer">
-    <div class="container">
+    <div class="container-fluid">
         <nav class="float-left">
-        <ul>
-            <li>
-            <a href="https://www.creative-tim.com">
-                {{ __('Creative Tim') }}
-            </a>
-            </li>
-            <li>
-            <a href="https://creative-tim.com/presentation">
-                {{ __('About Us') }}
-            </a>
-            </li>
-            <li>
-            <a href="http://blog.creative-tim.com">
-                {{ __('Blog') }}
-            </a>
-            </li>
-            <li>
-            <a href="https://www.creative-tim.com/license">
-                {{ __('Licenses') }}
-            </a>
-            </li>
-        </ul>
+            <ul>
+                <li>
+                    <a href="https://alekla.com/dashboard">
+                        {{ __('Alekla0126') }}
+                    </a>
+                </li>
+            </ul>
         </nav>
         <div class="copyright float-right">
-        &copy;
-        <script>
-            document.write(new Date().getFullYear())
-        </script>, made with <i class="material-icons">favorite</i> by
-        <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a> and <a href="https://www.updivision.com" target="_blank">UPDIVISION</a> for a better web.
+            &copy;
+            <script>
+                document.write(new Date().getFullYear())
+            </script>, hecho con <i class="material-icons">favorite</i> por
+            <a href="https://www.alekla.com" target="_blank">Alekla</a> y <a href="https://www.iberopuebla.mx" target="_blank">Universidad Iberomericana</a>
         </div>
     </div>
 </footer>
