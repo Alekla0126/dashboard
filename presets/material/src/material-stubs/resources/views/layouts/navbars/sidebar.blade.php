b0826ce2f9d51a54d5197f4ab97f678adb79495c
alekla0126 <alekla0126@gmail.com>
2021-06-03 19:17:37 -0500

    V1.8

--- presets/material/src/material-stubs/resources/views/layouts/navbars/sidebar.blade.php
+++ presets/material/src/material-stubs/resources/views/layouts/navbars/sidebar.blade.php
@@ -6,7 +6,7 @@
   -->
   <div class="logo">
     <a href="https://www.iberopuebla.mx" class="simple-text logo-normal">
-      {{ __('Dashboard para detectores de humo') }}
+      {{ __('Dashboard para detectores de humo en la Ibero') }}
     </a>
   </div>
   <div class="sidebar-wrapper">
