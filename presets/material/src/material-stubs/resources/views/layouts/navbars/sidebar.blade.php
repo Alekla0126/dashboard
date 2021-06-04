bfda9402d534d6656d0797eac39740d856dad5e7
alekla0126 <alekla0126@gmail.com>
2021-06-03 19:13:28 -0500

    V1.7

--- presets/material/src/material-stubs/resources/views/layouts/navbars/sidebar.blade.php
+++ presets/material/src/material-stubs/resources/views/layouts/navbars/sidebar.blade.php
@@ -5,8 +5,8 @@
       Tip 2: you can also add an image using data-image tag
   -->
   <div class="logo">
-    <a href="https://creative-tim.com/" class="simple-text logo-normal">
-      {{ __('Creative Tim') }}
+    <a href="https://www.iberopuebla.mx" class="simple-text logo-normal">
+      {{ __('Dashboard para detectores de humo') }}
     </a>
   </div>
   <div class="sidebar-wrapper">
