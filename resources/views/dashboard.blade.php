ad989949b2a9253cdb10a976d6b5f3813c1f50dd
alekla0126 <alekla0126@gmail.com>
2021-06-04 20:58:26 -0500

    V1.78

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -26,7 +26,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script>
         const gaugeElement = document.querySelector(".gauge");
-        let temp = JSON.parse('{{ json_encode($sensor->temperature) }}');
+        let temp = JSON.parse("{{ json_encode($sensor->temperature) }}");
         function setGaugeValue(gauge, value)
         {
             if (value < 0 || value > 1)
